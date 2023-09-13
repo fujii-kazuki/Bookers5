06c7599f6455de357d4f5667e06def5e315a39d5
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-09-13 11:48:18 +0000

    [Add] 課題問題に必要なファイルを追加

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "homes#top"
+  get "home/about"=>"homes#about"
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy, :following_users, :followed_users]
+    get "following" => "relationships#following", as: :following
+    get "followed" => "relationships#followed", as: :followed
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
